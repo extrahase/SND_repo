28100fd084a70b0b1990bff7ec7f247fa205accf
extrahase <christoph.bunge@outlook.com>
2025-07-14 01:14:29 +0200

    cleaned and fixed (hopefully)

--- Scripts/Fly to Flag.lua
+++ Scripts/Fly to Flag.lua
@@ -10,18 +10,11 @@ MOUNT_SPEED = 20.6
 TP_DELAY = 7
 
 local etaTp, closestAetheryteId = functions.CalculateEtaTp3()
-local etaFlight = functions.CalculateEtaFlight3()
 
 if Instances.Map.Flag.TerritoryId ~= Svc.ClientState.TerritoryType then
     Actions.Teleport(closestAetheryteId)
     functions.WaitForZoneAndReady()
     end
-
-if etaTp <= etaFlight then
-    functions.WaitForReady()
-    Actions.Teleport(closestAetheryteId)
-    functions.Wait(5)
-end
 
 functions.FlyToFlag()
 functions.Dismount()
