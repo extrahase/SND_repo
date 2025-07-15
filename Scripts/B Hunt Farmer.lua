e37aef2fa65fed1727b2e0136e306f3c11de0551
extrahase <christoph.bunge@outlook.com>
2025-07-15 19:19:29 +0200

    added one more waypoint for better routing

--- Scripts/B Hunt Farmer.lua
+++ Scripts/B Hunt Farmer.lua
@@ -42,7 +42,8 @@ functions.MountUp()
 local zoneHuntLocations = {
     { x = 22.2, y = 26.3 },
     { x = 23.1, y = 22.6 },
-    { x = 16.6, y = 16.1 }
+    { x = 16.6, y = 16.1 },
+    { x = 23.1, y = 22.6 }
 }
 
 -- loop through each flag, place it and start the hunt
