c6c51039682729ad3b446d5ada1f4c016e3c2dc5
extrahase <christoph.bunge@outlook.com>
2025-08-01 15:28:58 +0200

    excluded Tertium as valid in-zone tp option

--- Scripts/Hunt Train Passenger.lua
+++ Scripts/Hunt Train Passenger.lua
@@ -54,7 +54,7 @@ local etaTp, closestAetheryteId = functions.CalculateEtaTp3()
 local etaFlight = functions.CalculateEtaFlight3()
 
 if etaTp <= etaFlight then
-    if closestAetheryteId ~= 148 then
+    if closestAetheryteId ~= 148 and closestAetheryteId ~= 173 then
     functions.TpToAetheryte(closestAetheryteId)
     end
 end
