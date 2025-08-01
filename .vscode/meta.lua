13056c4c6a93ff288c2cd06d7c37efa1bd8d6ccf
extrahase <christoph.bunge@outlook.com>
2025-08-01 03:21:14 +0200

    changed meta.lua and settings.json to point to the new location of meta.lua

--- .vscode/meta.lua
+++ .vscode/meta.lua
@@ -2,3 +2,21 @@
 ---@field X number
 ---@field Y number
 ---@field Z number
+Vector3 = { }
+
+---@class Luanet
+luanet = {}
+
+---Loads a .NET assembly by name
+---@param name string
+function luanet.load_assembly(name) end
+
+---Imports a .NET type by fully qualified name
+---@param typeName string
+---@return any
+function luanet.import_type(typeName) end
+
+---Loads an enum or returns .NET constant value
+---@param enumName string
+---@return any
+function luanet.enum(enumName) end
