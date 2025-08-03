d62e9ce60a2639e0a5d1576e891034b901da5634
extrahase <christoph.bunge@outlook.com>
2025-08-03 03:18:33 +0200

    created script with section headers and outline

---
+++ Scripts/Replenish Maps.lua
@@ -0,0 +1,32 @@
+-- Replenish Maps
+
+-- ############
+-- ### DATA ###
+-- ############
+
+local functions = require("functions")
+
+DEBUG = true
+
+-- #################
+-- ### FUNCTIONS ###
+-- #################
+
+
+
+-- ############
+-- ### MAIN ###
+-- ############
+
+-- move to market board
+-- function buymap start
+    -- access market board
+    -- search for map
+    -- click on map
+    -- buy map
+    -- exit menus
+-- function end
+-- function deciphermap
+-- function buymap
+-- function storeinsaddlebag
+-- function buymap
