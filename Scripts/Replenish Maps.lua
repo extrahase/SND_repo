a46f8d8b24393eac7e382d7331d155cd7e6c1134
extrahase <christoph.bunge@outlook.com>
2025-08-15 04:05:51 +0200

    finished first iteration of script

--- Scripts/Replenish Maps.lua
+++ Scripts/Replenish Maps.lua
@@ -1,10 +1,10 @@
--- Replenish Maps
-
 -- ############
 -- ### DATA ###
 -- ############
 
-local functions = require("functions")
+local f = require("functions")
+
+MAP_NAME = "Timeworn Loboskin Map"
 
 DEBUG = true
 
@@ -12,21 +12,34 @@ DEBUG = true
 -- ### FUNCTIONS ###
 -- #################
 
-
+function DecipherMap(mapName)
+    f.WaitForReady()
+    yield("/item " .. mapName)
+    f.WaitForReady()
+end
 
 -- ############
 -- ### MAIN ###
 -- ############
 
--- move to market board
--- function buyitemfrommarketboard start
-    -- access market board
-    -- search for map
-    -- click on map
-    -- buy map
-    -- exit menus
--- function end
--- function deciphermap
--- function buyitemfrommarketboard
--- function storeinsaddlebag
--- function buyitemfrommarketboard
+f.Echo("Starting script!")
+
+f.Echo("Moving to Market Board")
+f.Lifestream("mb")
+
+f.Echo("Buying first " .. MAP_NAME .. " from Market Board")
+f.BuyItemFromMarketBoard(MAP_NAME)
+
+f.Echo("Deciphering first " .. MAP_NAME)
+DecipherMap(MAP_NAME)
+
+f.Echo("Buying second " .. MAP_NAME .. " from Market Board")
+f.BuyItemFromMarketBoard(MAP_NAME)
+
+f.Echo("Storing second " .. MAP_NAME .. " in saddlebag")
+f.StoreItemInSaddlebag(MAP_NAME)
+
+f.Echo("Buying third " .. MAP_NAME .. " from Market Board")
+f.BuyItemFromMarketBoard(MAP_NAME)
+
+f.Echo("Script done!")
