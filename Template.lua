1549f803f96023dc58f74d44846eaffcbd08f6b2
extrahase <christoph.bunge@outlook.com>
2025-08-20 16:08:25 +0200

    updated

--- Template.lua
+++ Template.lua
@@ -1,1 +1,26 @@
-local functions = require("functions")
+-- ############
+-- ### DATA ###
+-- ############
+
+import("System.Numerics")
+local f = require("functions")
+
+HUNT_MARKS = require("huntMarks")
+ZONE_LIST = require("vac_lists").Zone_List
+WORLD_ID_LIST = require("vac_lists").World_ID_List
+
+HUNT_RANK = "S"
+VBM_PRESET = "A Ranks"
+MOUNT_SPEED = 20
+RUN_SPEED = 6
+TP_DELAY = 11
+
+DEBUG = true
+
+-- #################
+-- ### FUNCTIONS ###
+-- #################
+
+-- ############
+-- ### MAIN ###
+-- ############
