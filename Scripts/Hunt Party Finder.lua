5730b7edeebdf189cc8d1cf5e2166926c193d835
extrahase <christoph.bunge@outlook.com>
2025-08-21 01:12:37 +0200

    first working version

--- Scripts/Hunt Party Finder.lua
+++ Scripts/Hunt Party Finder.lua
@@ -13,3 +13,29 @@ DEBUG = true
 -- ############
 -- ### MAIN ###
 -- ############
+
+f.Echo("Starting script!")
+
+f.Echo("Making sure we are party-less")
+f.LeaveParty()
+
+f.Echo("Opening Party Finder")
+f.CloseAddon("LookingForGroup")
+yield("/send OEM_3")
+f.WaitForAddon("LookingForGroup")
+
+f.Echo("Joining existing listing")
+-- WIP!
+
+f.Echo("Creating new listing")
+f.Callback2("LookingForGroup", 20, 0) -- navigates to Data Center tab
+f.Callback2("LookingForGroup", 21, 11) -- navigates to The Hunt tab
+f.SelectListOption("LookingForGroup", 14) -- clicks on Recruit Members
+f.WaitForAddon("LookingForGroupCondition")
+f.Callback2("LookingForGroupCondition", 12, 11) -- selects The Hunt as Duty
+f.Callback2("LookingForGroupCondition", 32, 1) -- selects Remove role restrictions for all remaining openings.
+f.SelectListOption("LookingForGroupCondition", 0) -- clicks Recruit Members
+f.CloseAddon("LookingForGroupCondition")
+f.CloseAddon("LookingForGroup")
+
+f.Echo("Script done!")
