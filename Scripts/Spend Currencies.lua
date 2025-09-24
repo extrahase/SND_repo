7b7b85b2a64374711b1c82f9597ef8669aff4fc8
extrahase <christoph.bunge@outlook.com>
2025-09-24 03:32:33 +0200

    changed Waxing Arcanite max amount

--- Scripts/Spend Currencies.lua
+++ Scripts/Spend Currencies.lua
@@ -174,7 +174,7 @@ local function SpendUncapped()
     end
 
     f.Echo("Checking item max quantity")
-    if Inventory.GetItemCount(46850) >= 57 then
+    if Inventory.GetItemCount(46850) >= 54 then
         f.Error("Relic material is at cap, cannot spend Uncapped")
         return
     end
