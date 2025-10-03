5c8b368249f870e6a8cbd40bf0c4347fc86ad417
extrahase <christoph.bunge@outlook.com>
2025-10-04 00:15:24 +0200

    created

---
+++ Scripts/Sphere Scroll.lua
@@ -0,0 +1,22 @@
+-- ############
+-- ### DATA ###
+-- ############
+
+import("System.Numerics")
+local f = require("functions")
+
+DEBUG = true
+
+-- #################
+-- ### FUNCTIONS ###
+-- #################
+
+-- ############
+-- ### MAIN ###
+-- ############
+
+f.Echo("Starting script!")
+
+
+
+f.Echo("Script done!")
